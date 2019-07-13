15c25020dcd6a3128bf8c35da9fd4d18c97f3e2c
mwbouwkamp <mwbouwkamp@gmail.com>
2019-07-13 14:05:21 +0200

    Solved Day 14 - second puzzle

--- src/Day14/Day14.java
+++ src/Day14/Day14.java
@@ -30,27 +30,35 @@ public class Day14 {
 
 
         recipes = new ArrayList<>();
+        String recipesString = "37";
         recipes.add(3);
         recipes.add(7);
         elf1 = 0;
         elf2 = 1;
         String input2 = "110201";
+        int counter = input2.length() - 8;
         while (true) {
             int newRecipe = recipes.get(elf1) + recipes.get(elf2);
             if (newRecipe >= 10) {
                 recipes.add(newRecipe / 10);
+                recipesString += newRecipe / 10;
                 recipes.add(newRecipe % 10);
+                recipesString += newRecipe % 10;
+                counter += 2;
             }
             else {
                 recipes.add(newRecipe);
+                recipesString += newRecipe;
+                counter += 1;
             }
-            String recipeString = "";
-            for (Integer recipe: recipes) {
-                recipeString += recipe;
-            }
-            if (recipeString.contains(input2)) {
-                System.out.println(recipeString.indexOf(input2));
+            if (recipesString.contains(input2)) {
+                System.out.println(recipesString.indexOf(input2));
+                System.out.println(recipesString);
+                System.out.println(recipes.size());
                 break;
+            }
+            if (recipesString.length() - 5 > -1) {
+                recipesString = recipesString.substring(recipesString.length() - 5);
             }
             elf1 = (elf1 + recipes.get(elf1) + 1) % recipes.size();
             elf2 = (elf2 + recipes.get(elf2) + 1) % recipes.size();
